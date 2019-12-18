d2e2d9f35d7c0986763e57c47ff030e1c81fb321
Pat Palmerston <patpalmerston@gmail.com>
2019-12-17 16:36:30 -0800

    for each working mvp day three

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -228,7 +228,13 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
+        if not self.right and not self.left:
+            return
 
     # DAY 2 Project -----------------------
 
